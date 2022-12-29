c8ae64115df2beddeedeeade373dacf444d583bc
anont <anont@>
2022-12-29 03:39:25 +0200

    Changed the way of storing and using tiles (coordinates). Started to work with View and VM

--- MapServices/Map.cs
+++ MapServices/Map.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,17 +8,15 @@ using TableGame.Units;
 
 namespace TableGame.MapServices
 {
-    /// <summary>
-    /// 
-    /// </summary>
+
     internal class Map
     {
         private readonly string name;
-        private readonly Tile[,] tiles;
+        private readonly List<List<Tile>> tiles;
         //private readonly List<Tile> tiles = new();
         private readonly int size_x, size_y;
         public string Name { get => name; }
-        public Tile[,] Tiles { get => tiles; }
+        public List<List<Tile>> Tiles { get => tiles; }
         //public List<Tile> Tiles { get => tiles; }
         public int Size_x { get => size_x; }
         public int Size_y { get => size_y; }
@@ -37,8 +36,10 @@ namespace TableGame.MapServices
         }
 
         [System.Text.Json.Serialization.JsonConstructor]
-        public Map(int size_x, int size_y, string name, Tile[,] coordinates)
+        public Map(int size_x, int size_y, string name, List<List<Tile>> coordinates)
         {
+            tiles = new();
+
             this.name = name;
             this.size_x = size_x;
             this.size_y = size_y;
@@ -46,16 +47,22 @@ namespace TableGame.MapServices
         }
 
         //public Map()
-        public Tile[,] TileCreator(int size_x, int size_y)
+        public List<List<Tile>> TileCreator(int size_x, int size_y)
         {
-            Tile[,] newMap = new Tile[size_x, size_y];
-            for (int i = 0; i < size_x; i++)
+            var newMap = new List<List<Tile>>();
+
+            for (int x = 0; x < size_x; x++)
             {
-                for (int j = 0; j < size_y; j++)
+                var newListX = new List<Tile>();
+
+                for (int y = 0; y < size_y; y++)
                 {
-                    newMap[i, j] = new Tile(i, j);
+                    //newMap[i, j] = new Tile(i, j);
                     //tiles.Add(newMap[i, j]);
+                    newListX.Add(new Tile(x, y));
                 }
+
+                newMap.Add(newListX);
             }
             return newMap;
         }
@@ -64,14 +71,14 @@ namespace TableGame.MapServices
         {
             int x = o.PosX;
             int y = o.PosY;
-            tiles[x, y].TileObject = o;
+            //tiles[x, y].TileObject = o;
         }
 
         public void RemoveObject(MapObject o)
         {
             int x = o.PosX;
             int y = o.PosY;
-            tiles[x, y].TileObject = null;
+            //tiles[x, y].TileObject = null;
         }
 
         public void SetTileType(int x, int y, string type)
