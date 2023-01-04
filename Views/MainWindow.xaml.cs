c1ecc48e69155e19efb07fe281f985ed923617ac
Slavyan07 <Slavyan07@>
2023-01-04 09:00:37 +0200

    Edit: add list logs mainwindow switch depence start app mode (debug)

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -22,11 +22,19 @@ namespace TableGame.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowVM _dataContext;
+
         public MainWindow(Player player1, Player player2, int totalSteps)
         {
             InitializeComponent();
 
-            DataContext = new MainWindowVM(player1, player2, totalSteps);
+            _dataContext = new MainWindowVM(player1, player2, totalSteps);
+            DataContext = _dataContext;
+
+#if DEBUG
+            // DEBUG LIST IN WINDOW
+            ListTrackPlayers.ItemsSource = _dataContext.Logger.Logs;
+#endif
 
         }
 
