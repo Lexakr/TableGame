1e217491856dd5012b606726d8953ff6b67d0ae2
Lexakr <alexeynealeksey@gmail.com>
2023-01-08 23:00:06 +0700

    new structure: rock

---
+++ MapServices/Rock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableGame.MapServices
+{
+    internal class Rock : Structure
+    {
+        public Rock()
+        {
+            Icon = "/Resources/Rocks/rock" + new Random().Next(1,5).ToString() + ".png";
+        }
+    }
+}
